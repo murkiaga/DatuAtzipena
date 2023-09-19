41dd5bfab3d982fb8c792cbeaa59259cfab150fc
murkiaga <murkiaga001@gmail.com>
2023-09-19 08:33:21 +0200

    zuzenketa sarbide zuzenean

--- klaseko_ariketak/1-TestuFitxategiak/FicheroAccesoAleatorio.java
+++ klaseko_ariketak/1-TestuFitxategiak/FicheroAccesoAleatorio.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.ArrayList; 
 import java.util.HashMap;
 import java.util.Map;
-import javafx.util.*;
+import javafx.util.Pair;
 
 public class FicheroAccesoAleatorio {
 
